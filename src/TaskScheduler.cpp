83331452a2590194c96164ea3470c5b432d652d1
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2026-05-09 10:36:13 +1000

    code cleanup

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -45,14 +45,6 @@ void TaskScheduler::plan(int time_limit, std::vector<int> & proposed_schedule)
     }
     else if (solver == 3)
     {
-        DefaultPlanner::schedule_plan_matching(limit, proposed_schedule, env, background_flow, use_traffic, new_only, max_matching_edges);
-    }
-    else  if (solver == 4)
-    {
-        DefaultPlanner::schedule_plan_h(limit, proposed_schedule, env, new_only);
-    }
-    else  if (solver == 5)
-    {
         DefaultPlanner::schedule_plan_raw(limit, proposed_schedule, env);
     }
     else
